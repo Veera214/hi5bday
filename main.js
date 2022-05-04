3fc84bf94c20b1e8b89b49af912cd16528a6132a
Veera214 <82866617+Veera214@users.noreply.github.com>
2022-05-04 12:52:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+var headcount=0;
+var hc=0;
+yes_image= "yes.png";
+no_image="https://image.shutterstock.com/image-vector/miss-you-vector-sign-on-600w-618418850.jpg";
+
+
+function playSound(){
+	x.play();
+}
